51e1958d1d4976403fcc43b8eba2693bb5966c6e
JuliaSerrano <jserranoarrese@gmail.com>
2022-08-02 14:15:31 +0200

    Extract data, export to csv

--- API_endpoint_method.py
+++ API_endpoint_method.py
@@ -2,18 +2,46 @@ import requests
 import pandas as pd
 import json
 import math
+from request import request
+
+link = 'https://www.fotocasa.es'
 
 
+
+#open and load json from raw responde (obtained with insomnia)
 with open('fotocasa_1_alcobendas.json') as f:
     jsondata = json.load(f)
 
 
-
-
 #numero de viviendas
 num_viv = jsondata['count']
-print(num_viv)
 
 #numero de paginas
 num_pags = math.ceil(num_viv/30)
-print(num_pags)
+
+# data = request(num_pags)
+# jsondata = data[0]
+
+
+#data to extract
+url =[]
+mobile = []
+real_estate = []
+
+#iterate for each property
+for home in jsondata['realEstates']:
+    #extract url
+    url.append(link + home['detail']['es'])
+
+    #extract mobile
+    mobile.append(home['advertiser']['phone'])
+
+    #extract real estate
+    real_estate.append(home['advertiser']['clientAlias'])
+
+#create data frame and export to csv
+df = pd.DataFrame(data={"link":url,"mobile":mobile})
+df.to_csv("./file.csv", sep=',',index=False)
+print(df)
+
+
