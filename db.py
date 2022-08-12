4e225625c9082c17ea30164841bd64b07e66fdb5
JuliaSerrano <jserranoarrese@gmail.com>
2022-08-12 15:03:54 +0200

    Database insert methods and modify tables

--- db.py
+++ db.py
@@ -31,19 +31,60 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
+
+
+def insert_property(conn,property):
+    """ insert property into the property table
+    :param conn: Connection object
+    :param property: 
+    :return property id
+    """
+    sql =  ''' INSERT INTO properties(date, retrieved_date, url, mobile, agency, real_estate_id)
+              VALUES(?,?,?,?,?,?) '''
+
+    c = conn.cursor()
+    c.execute(sql,property)
+    conn.commit()
+    return c.lastrowid
+
+def insert_price_history(conn,price_history):
+    """ insert price_history into price_history table
+    :param conn: Connection object
+    :param price_history: 
+    :return property id
+    """
+    sql =  ''' INSERT INTO price_history(price, retrieved_date, createdAt, updatedAt)
+              VALUES(?,?,?,?) '''
+
+    c = conn.cursor()
+    c.execute(sql,price_history)
+    c.commit()
+    return c.lastrowid
+    
+
 def main():
-    database = 'real_estate_tracker.db'
+    database = 'realEstateTracker.db'
 
     sql_create_properties_table = """ CREATE TABLE IF NOT EXISTS properties(
-        id INTEGER PRIMARY KEY,
-        date TEXT,
-        retrieved_date TEXT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date DATETIME,
+        retrieved_date DATETIME,
         url TEXT NOT NULL,
         mobile INTEGER,
-        real_estate text,
-        typeId INTEGER NOT NULL
+        agency TEXT,
+        real_estate_id INTEGER NOT NULL
     ); """
 
+    
+    sql_create_price_history_table=""" CREATE TABLE IF NOT EXISTS price_history(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        price INTEGER,
+        retrieved_date DATETIME,
+        createdAt DATETIME,
+        updatedAt DATETIME,
+		FOREIGN KEY(retrieved_date) REFERENCES properties(retrieved_date),
+		FOREIGN KEY(id) REFERENCES properties(id)
+    ); """
 
 
     # create a database connection
@@ -51,11 +92,22 @@ def main():
 
     # create tables
     if conn is not None:
-        # create projects table
+        # create properties table
         create_table(conn, sql_create_properties_table)
+
+        #create price_history table
+        create_table(conn, sql_create_price_history_table)
 
     else:
         print("Error! cannot create the database connection.")
+
+    #insert into tables
+    
+    #create elements to test the functions
+        #date, retrieved_date, url, mobile, agency, real_estate_id)
+    property = (('2007-01-01 10:00:00','2021-04-02 10:00:00','www.youtube.com',6431345,'Engelk',987643))
+    property_id = insert_property(conn,property)
+    print(property_id)
 
 
 if __name__ == '__main__':
