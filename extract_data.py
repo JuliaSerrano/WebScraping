cc134056733f7170326977422bc3ab40fc6bc244
Julia <jserranoarrese@gmail.com>
2022-09-03 00:39:39 +0200

    Fix locations

--- extract_data.py
+++ extract_data.py
@@ -2,44 +2,48 @@
 
 link = 'https://www.fotocasa.es'
 
-def extract_data(jsondata,url,mobile,real_estate,type_id,date,real_estate_id,price,trans_type_id,location,query_param):
 
-    
-    #iterate for each property
+def extract_data(jsondata, url, mobile, real_estate, type_id, date, real_estate_id, price, trans_type_id, location, query_param):
+
+    # iterate for each property
     for home in jsondata['realEstates']:
-        #extract url
+        # extract url
         url.append(link + home['detail']['es'])
 
-        #extract mobile
+        # extract mobile
         mobile.append(home['advertiser']['phone'])
 
-        #extract real estate
+        # extract real estate
         real_estate.append(home['advertiser']['clientAlias'])
 
-        #type id (if 1 -> agency notlinked)
+        # type id (if 1 -> agency notlinked)
         type_id.append(home['advertiser']['typeId'])
 
-        #date after T, it gets time. Only get date
+        # date after T, it gets time. Only get date
         date.append((home['date']).partition("T")[0])
 
-        #extract real estate id
+        # extract real estate id
         real_estate_id.append((home['id']))
 
-        #extract price
+        # extract price
         price.append(int(home['transactions'][0]['value'][0]))
 
-        #transactionTypeId
-        #1 -> Buy  3 -> Rent
+        # transactionTypeId
+        # 1 -> Buy  3 -> Rent
         trans_type_id.append(home['transactions'][0]['transactionTypeId'])
 
-        #location
-        level5 =  home['address']['location']['level5']
-        if level5 ==  " Madrid Capital":
-            level7 = home['address']['location']['level7']
+        # location
+        level5 = home['address']['location']['level5']
+        level7 = home['address']['location']['level7']
+
+        if level5 == " Madrid Capital":
+            if level7 == "Hortaleza" or level7 == "Fuencarral - El Pardo":
+                level8 = home['address']['location']['level8']
+                location.append(level8)
+            else:
+                location.append(level5)
+
+        elif level5 == "La Moraleja" and level7 == "Arroyo de la Vega":
             location.append(level7)
         else:
             location.append(level5)
-
-
-
-        
