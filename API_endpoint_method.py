486bcc330073151a98385d3e32df37672d21c1c5
Julia <jserranoarrese@gmail.com>
2022-11-07 18:18:18 +0100

    Problem deploy

--- API_endpoint_method.py
+++ API_endpoint_method.py
@@ -70,7 +70,7 @@ def main():
     for jsondata in data:
         extract_data(jsondata, url, mobile, real_estate, type_id, date,
                      real_estate_id, price, trans_type_id, location, num_days)
-        # print(f"{url}\n")
+
         # export_csv("./file.csv",url,mobile,real_estate)
     # export_excel('todoJunto.xlsx', url, mobile, real_estate, type_id,
         #  date, real_estate_id, price, trans_type_id, location, num_days)
